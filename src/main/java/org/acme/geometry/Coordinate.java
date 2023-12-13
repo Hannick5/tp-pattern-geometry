60e602b1585eee1ee96bf15d65c025bf7d1129c4
Hannick5 <abdul.hannick@gmail.com>
2023-12-13 17:07:33 +0100

    ajout coordinate bon

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,45 +1,34 @@
 package org.acme.geometry;
 
 public class Coordinate {
-	
-	private double x = Double.NaN;
-	private double y = Double.NaN;
-	
-public Coordinate() {
-	
-}
+	private double x;
 
-public Coordinate(double x, double y) {
-	super();
-	this.x = x;
-	this.y = y;
-}
+	private double y;
 
-public double getX() {
-	return x;
-}
-
-
-public double getY() {
-	return y;
-}
-
-
-public boolean isEmpty() {
-	 if (Double.isNaN(x) || Double.isNaN(y)) {
-		return true;
-	} else {
-		return false;
+	public Coordinate() {
+		this.x = Double.NaN;
+		this.y = Double.NaN;
 	}
-}
 
-@Override
-public String toString() {
-    if (isEmpty()) {
-        return "[NaN,NaN]";
-    } else {
-        return "["+ x +","+ y +"]";
-    }
-}
+	public Coordinate(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
 
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + x + "," + y + "]";
+	}
+
+	public boolean isEmpty() {
+		return Double.isNaN(x) || Double.isNaN(y);
+	}
 }
--- src/test/java/org/acme/geometry/CoordinateTest.java
+++ src/test/java/org/acme/geometry/CoordinateTest.java
@@ -1,5 +1,9 @@
 package org.acme.geometry;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,16 +12,23 @@ public class CoordinateTest {
 	public static final double EPSILON = 1.0e-15;
 
 	@Test
-	public void testConstructorXY(){
-		// TODO
-		Coordinate c = new Coordinate(3.0,4.0);
-		Assert.assertEquals(3.0, c.getX(), EPSILON);
-		Assert.assertEquals(4.0, c.getY(), EPSILON);
-		Assert.assertFalse(c.isEmpty());
-        Assert.assertEquals("[3.0,4.0]", c.toString());
-        
-        Coordinate d = new Coordinate();
-        Assert.assertEquals("[NaN,NaN]", d.toString());
+	public void testCoordinateXY() {
+		
+		Coordinate c = new Coordinate(3.0, 4.0);
+		Coordinate c2 = new Coordinate();
+		Coordinate c3 = new Coordinate(Double.NaN, 4.0);
+		
+		assertEquals(3.0, c.getX(), EPSILON);
+		assertEquals(4.0, c.getY(), EPSILON);
+		
+		assertTrue(Double.isNaN(c2.getX()));
+		assertTrue(Double.isNaN(c2.getY()));
+		
+		assertFalse(c.isEmpty());
+		assertEquals("[3.0,4.0]", c.toString());
+		
+		assertTrue(c3.isEmpty());
+		
 	}
 
 }
