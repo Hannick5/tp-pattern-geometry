cdbad580876a7b926b717b16dcce5ad1a15781cd
Hannick5 <abdul.hannick@gmail.com>
2023-12-15 13:55:31 +0100

    adding the wktwriter

---
+++ src/main/java/org/acme/geometry/WktWriter.java
@@ -0,0 +1,33 @@
+package org.acme.geometry;
+
+public class WktWriter {
+
+    public String write(Geometry geometry) {
+        String result = geometry.getType().toUpperCase();
+
+        if (geometry.isEmpty()) {
+            result += " EMPTY";
+        } else {
+
+            if (geometry instanceof Point) {
+                Point point = (Point) geometry;
+                result += "(" + point.getCoordinate().getX() + " " + point.getCoordinate().getY() + ")";
+            } else if (geometry instanceof LineString) {
+                LineString lineString = (LineString) geometry;
+                result += "(";
+                for (int i = 0; i < lineString.getNumPoints(); i++) {
+                    Point p = lineString.getPointN(i);
+                    result += p.getCoordinate().getX() + " " + p.getCoordinate().getY();
+                    if (i < lineString.getNumPoints() - 1) {
+                        result += ",";
+                    }
+                }
+                result += ")";
+            } else {
+                //throw new RuntimeException("geometry type not supported");
+            }
+        }
+        return result;
+    }
+}
+
---
+++ src/test/java/org/acme/geometry/WktwriterTest.java
@@ -0,0 +1,35 @@
+package org.acme.geometry;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class WktwriterTest {
+	@Test
+	public void TestWriter() {
+		Coordinate c1 = new Coordinate(3.0, 4.0);
+		Coordinate c2 = new Coordinate(2.0, 5.0);
+		Coordinate c3 = new Coordinate(6.0, 9.0);
+
+		Point p1 = new Point(c1);
+		Point p2 = new Point(c2);
+		Point p3 = new Point(c3);
+		
+		List<Point> listPoint = Arrays.asList(p1, p2, p3);
+		
+		LineString l = new LineString(listPoint);
+		WktWriter w = new WktWriter();
+		
+		assertEquals(w.write(p1), "POINT(3.0 4.0)");
+		assertEquals(w.write(l), "LINESTRING(3.0 4.0,2.0 5.0,6.0 9.0)");
+		
+		Point p = new Point();
+		l = new LineString();
+		
+		assertEquals(w.write(p), "POINT EMPTY");
+		assertEquals(w.write(l), "LINESTRING EMPTY");
+	}
+}
